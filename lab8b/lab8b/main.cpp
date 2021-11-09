7b41e837e7a5bbe401382799b0499acb3d6c02f4
Andrey <andreykaraulanov@icloud.com>
2021-11-09 21:19:25 +0200

    дфи8и

--- lab8b/lab8b/main.cpp
+++ lab8b/lab8b/main.cpp
@@ -1,22 +1,32 @@
-//#define _CRT_SECURE_VO_WARNINGS;
 #include <iostream>
 using namespace std;
-int main() {
-    int n = 5;
-    int x[n];
-    int i, j, k;
-    cout<<"Input"<<n<<"numbers:"<<endl;
+
+int n;
+
+void udalenie(int *x, int s)
+{
+    for(; s<n; s++)
+        x[s] = x[s+1];
+    n--;
+    x = (int *)malloc(n*sizeof(int));
+}
+
+int main()
+{
+    int *x, i, s, po;
+    cout<<"Razmer massiva: "; cin>>n;
+    x = (int *)malloc(n*sizeof(int));
     for(i=0; i<n; i++)
-        cin >> x[i];
-    k = 0;
-    for (i=0; i<n; i++) {
-        if (!x[i]<0) {
-            x[k]=x[i];
-            k++;
+        {
+            cout<<"X["<<i<<"]="; cin>>x[i];
         }
-    }
-    cout<<"Otvet:"<<endl;
-    for(i=0; i<k;i++)
-        cout<< x[i]<<endl;
+    cout<<"Ydalit' vse chisla massiva s: "; cin>>s; // вводишь индекс с какого
+    cout<<"Ydalit' vse chisla massiva po: "; cin>>po; // вводишь индекс по какое
+    for(; s<=po; s++)
+        udalenie(x, s);
+    cout<<"Massiv posle transformacii: ";
+    for(i=0; i<n; i++)
+        cout<<x[i]<<" ";
+    cout<<endl;
     return 0;
 }
