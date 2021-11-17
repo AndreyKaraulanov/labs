5dbe3e3ace9f72b916a7079b44780168509fe1d3
Andrey <andreykaraulanov@icloud.com>
2021-11-17 13:18:59 +0200

    lab9

--- lab9/lab9/main.cpp
+++ lab9/lab9/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
+#include <iomanip>
+#include <string>
+#include <set>
 using namespace std;
 #define S 9
-int Ar[S][S];
 
-int main(int argc, const char * argv[]) {
-    
+int main() {
+      
+    int Ar[S][S] ={};
+    int c = 1;
+    for (int j = 0; j < S; j++) {
+        for (int i = 0; i < S; i++) {
+            if((j<i && i<S/2) || (j<S-i-1 && i>=S/2) || (j>S-i-1 && i<S/2) || (j>i && i>=S/2))
+                    Ar[i][j]=c++;
+            else
+                    Ar[i][j]=0;
+        }
+    }
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < S; j++) {
+            cout << setw(4) << Ar[i][j];
+        }
+        cout<<endl;
+    }
     return 0;
 }
+
