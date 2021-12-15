1fd53503f701627ac6c3cbbf84718c51672d0240
Andrey <andreykaraulanov@icloud.com>
2021-12-15 10:52:29 +0200

    Graphical tasks

---
+++ Graphical tasks/Graphical task 1/Graphical task 1/main.cpp
@@ -0,0 +1,208 @@
+#include<iostream>
+#include<ctime>
+#include<cmath>
+#include<iomanip>
+#include <chrono>
+#define M 100
+using namespace std;
+
+
+
+int Gnome_sort(int arr[], int size){
+    int index = 0;
+    while (index < size) {
+        if (index == 0) {
+            index++;
+        }
+        if (arr[index] >= arr[index - 1]) {
+            index++;
+        }
+        else{
+            int t = arr[index];
+            arr[index] = arr[index-1];
+            arr[index-1]=t;
+            index--;
+        }
+    }
+    return arr[size];
+}
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    // Инициализируем наибольший элемент как корень
+        int l = 2*i + 1; // левый = 2*i + 1
+        int r = 2*i + 2; // правый = 2*i + 2
+
+     // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+       // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // Если самый большой элемент не корень
+        if (largest != i)
+        {
+            swap(arr[i], arr[largest]);
+
+    // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
+    }
+
+int heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i=n-1; i>=0; i--)
+    {
+        
+        int t = arr[0];
+        arr[0] = arr[i];
+        arr[i] = t;
+
+        
+        heapify(arr, i, 0);
+    }
+    return arr[n];
+}
+int main()
+{
+    while (true) {
+
+        cout.setf(ios::fixed);
+        cout.precision(9);
+
+        
+        int size;
+        cout << "Введите размер массива: ";
+        cin >> size;
+
+        int arr[1000];
+
+        cout << "Несортированный массив: " << endl;
+        for (int i = 0; i < size; i++) {
+
+            arr[i] = rand() % 10 + 1;
+            //arr[i] = i;
+            cout << setw(3) << arr[i];
+
+        }
+        /*for (int i = 0; i < size; i++) {
+
+                    arr[i] = size - i - 1;
+                    cout << setw(3) << arr[i];
+
+        }*/
+        
+        char ans;
+        cout<< endl <<"Какую сортировку вы хотите использовать гномью или пирамидальную?(g/h)";
+        cin>>ans;
+        char gnome;
+        gnome = 'g';
+        char heap;
+        heap = 'h';
+        
+        
+        int prep_arr[100];
+        int arr_sorted[100];
+            for (int i = 0; i < size; ++i) {
+                prep_arr[i] = arr[i];
+                //cout << setw(3) << arr[i];
+        }
+        
+        
+        
+        double dur_arr[M];
+        double average = 0;
+        if (ans == gnome) {
+            for (int i = 0; i < M; i++)
+            {
+
+                auto t1 = chrono::high_resolution_clock::now();
+
+                Gnome_sort(arr, size);
+
+                auto t2 = chrono::high_resolution_clock::now();
+                chrono::duration<double> duration = t2 - t1;
+
+                dur_arr[i] = duration.count();
+
+                cout << endl <<"Сортированный массив:" << endl;
+                for (int i = 0; i < size; i++) {
+                    cout << setw(3) << arr[i];
+                }
+                
+                
+                
+                /*for (int i = 0; i < size; ++i) {
+                    arr_sorted[i] = arr[i] ;
+                    //  cout << setw(3) << arr[i];
+                }*/
+                cout << endl << "Несортированный массив:" << endl;
+                for (int i = 0; i < size; ++i) {
+                    arr[i] = prep_arr[i];
+                    cout << setw(3) << arr[i];
+                }
+                
+
+            }
+        }
+        
+        else if (ans == heap) {
+            for (int i = 0; i < M; i++)
+            {
+
+                auto t1 = chrono::high_resolution_clock::now();
+
+                heapSort(arr, size);
+
+                auto t2 = chrono::high_resolution_clock::now();
+                chrono::duration<double> duration = t2 - t1;
+
+                dur_arr[i] = duration.count();
+
+                cout << endl << "Сортированный массив:" << endl;
+                for (int i = 0; i < size; i++) {
+                    cout << setw(3) << arr[i];
+                }
+                cout<<endl;
+                for (int i = 0; i < size; ++i) {
+                    arr[i] = prep_arr[i];
+                    cout << setw(3) << arr[i];
+                }
+            }
+        }
+
+        /*for (int i = 0; i < M; i++) {
+            cout << endl <<"Время выполнения " << i + 1 << " = " << dur_arr[i] << "сек." << endl;
+        }*/
+
+        double temp = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M - i - 1; j++) {
+                if (dur_arr[j] > dur_arr[j + 1]) {
+                    temp = dur_arr[j];
+                    dur_arr[j] = dur_arr[j + 1];
+                    dur_arr[j + 1] = temp;
+                }
+            }
+       }
+
+
+        for (int i = ((M / 100) * 40); i < M - ((M / 100) * 40); ++i) {
+            average += dur_arr[i];
+        }
+        
+        average = average / ((M / 100) * 20);
+
+        cout << endl <<"Время:" << average << "сек." << endl;
+
+
+    }
+
+    return 0;
+    
+}
