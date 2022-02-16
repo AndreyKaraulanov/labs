a6963e878f01167e2db1f3a542141f8152dbf96b
Andrey <andreykaraulanov@icloud.com>
2022-02-16 11:08:06 +0200

    2

---
+++ labs(2nd term)/lab 1/lab 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab 1
+//
+//  Created by Андрей Карауланов on 16.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
