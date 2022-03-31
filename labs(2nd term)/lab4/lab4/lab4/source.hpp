a09c0f48cdb70911e11557b0cc994a2d1ffad9d4
Andrey <andreykaraulanov@icloud.com>
2022-03-31 19:54:40 +0300

    labs

---
+++ labs(2nd term)/lab4/lab4/lab4/source.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#define SOLVE
+#define DEBUG
+using namespace std;
+void Array(int* pArr_A, int size, bool a);
+int Negative_numbers(int* pArr_A, int size,  int k);
+int Negative_numbers_action(int* pArr_A, int size, int k, int* pArr_B);
+int Replacement_max(int* pArr_A, int size,int max);
+int Replacement(int* pArr_A, int size, int max);
